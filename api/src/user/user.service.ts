bcf16f3176ff9741db1c3b553ba4fc0af714e986
jhlee <jhlee@spicusinc.com>
2025-06-05 18:02:34 +0900

    기능: DB 대신 Mock으로 변경

--- api/src/user/user.service.ts
+++ api/src/user/user.service.ts
@@ -1,55 +1,51 @@
 import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
-import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
-  constructor(
-    @InjectRepository(User)
-    private readonly userRepository: Repository<User>,
-  ) {}
+  private users: User[] = []
+  private idCounter = 1
 
   async create(createUserDto: CreateUserDto) {
     if (createUserDto.PWD !== createUserDto.CONFIRM_PWD) {
       throw new BadRequestException('비밀번호가 일치하지 않습니다.');
     }
 
+    const exists = this.users.find(user => user.ID === createUserDto.ID);
+    if (exists) {
+      throw new BadRequestException('이미 존재하는 사용자입니다.');
+    }
+
     const user = new User();
+    user.IDX = this.idCounter++;
     user.ID = createUserDto.ID;
     user.PWD = createUserDto.PWD;
     user.USE_PWD = createUserDto.USE_PWD;
     user.NAME = createUserDto.NAME;
     user.NICK_NAME = createUserDto.NICK_NAME;
+    user.CREATE_DATE = Date.now();
+    user.UPDATE_DATE = Date.now();
 
-    const savedUser = await this.userRepository.save(user);
-    const { PWD: removedPwd, USE_PWD: removedUsePwd, ...userWithoutPwd } = savedUser;
-    return userWithoutPwd;
+    this.users.push(user);
+    return this.removeSensitiveFields(user);
   }
 
   async findAll() {
-    const users = await this.userRepository.find();
-    const usersWithoutPwd = users.map((user) => {
-      const { PWD: removedPwd, USE_PWD: removedUsePwd, ...userWithoutPwd } = user;
-      return userWithoutPwd;
-    });
-    return usersWithoutPwd;
+    return this.users.map((user) => this.removeSensitiveFields(user));
   }
 
   async findOne(id: string, pwd: string) {
-    const user = await this.userRepository.findOne({ where: { ID: id, PWD: pwd } });
-    if (!user) {
+    const user = this.users.find((user) => user.ID === id);
+    if (!user || user.PWD !== pwd) {
       throw new NotFoundException('사용자를 찾을 수 없습니다.');
     }
-
-    const { PWD: removedPwd, USE_PWD: removedUsePwd, ...userWithoutPwd } = user;
-    return userWithoutPwd;
+    return this.removeSensitiveFields(user);
   }
 
   async update(idx: number, updateUserDto: UpdateUserDto) {
-    const user = await this.userRepository.findOne({ where: { IDX: idx } });
+    const user = this.users.find((user) => user.IDX === idx);
     if (!user) {
       throw new NotFoundException('사용자를 찾을 수 없습니다.');
     }
@@ -61,18 +57,21 @@ export class UserService {
     user.NICK_NAME = updateUserDto.NICK_NAME;
     user.UPDATE_DATE = Date.now();
 
-    const savedUser = await this.userRepository.save(user);
-    const { PWD: removedPwd, USE_PWD: removedUsePwd, ...userWithoutPwd } = savedUser;
-    return userWithoutPwd;
+    return this.removeSensitiveFields(user);
   }
 
   async remove(idx: number) {
-    const user = await this.userRepository.findOne({ where: { IDX: idx } });
-    if (!user) {
+    const index = this.users.findIndex((user) => user.IDX === idx);
+    if (index === -1) {
       throw new NotFoundException('사용자를 찾을 수 없습니다.');
     }
 
-    await this.userRepository.delete(idx);
+    this.users.splice(index, 1);
     return { message: '사용자가 삭제되었습니다.' };
+  }
+  
+  private removeSensitiveFields(user: User): Omit<User, 'PWD' | 'USE_PWD'> {
+    const { PWD, USE_PWD, ...safeUser } = user;
+    return safeUser;
   }
 }
