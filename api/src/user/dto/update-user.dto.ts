b5295b310d2d35d5ae3587566ff40876d19f1408
jhlee <jhlee@spicusinc.com>
2025-06-10 13:48:51 +0900

    기능: User Api 개선

--- api/src/user/dto/update-user.dto.ts
+++ api/src/user/dto/update-user.dto.ts
@@ -1,6 +1,9 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {
-  NICK_NAME: string;
+export class UpdateUserDto {
+  @ApiProperty({ example: 1, description: "사용자 인덱스" })
+  idx: number;
+
+  @ApiProperty({ example: "홍길동", description: "사용자 닉네임" })
+  nickname: string;
 }
